bd04d19f52a8ba067eaa1114e27501e629bccd3b
Edrei Hernández <edreihernandez.56@gmail.com>
2022-05-31 20:33:53 -0500

    Primera mitad de la clase 2

---
+++ src/bases-node/multi-nodemon.js
@@ -0,0 +1,16 @@
+'use strict'
+
+let cont = 0;
+
+const multiplicacion = (numb1, numb2) => {
+    let result = 0;
+    for (cont = 0; cont < numb2; cont++) {
+        result += numb1
+
+    }
+    return console.log(result)
+}
+
+multiplicacion(5, 8);
+
+
